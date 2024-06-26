129086c74bb37421ca70ddbd92256745063fc026
luciano <nicolini.luciano@hotmail.com>
2024-06-26 12:36:46 -0300

    Initial Commit

---
+++ DolarToArg/DolarToArgApp.swift
@@ -0,0 +1,17 @@
+//
+//  DolarToArgApp.swift
+//  DolarToArg
+//
+//  Created by Luciano Nicolini on 26/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct DolarToArgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
